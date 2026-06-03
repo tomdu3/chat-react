e9551dc2ebbcb60e5157a7eb4485a44137e7b62c
Tomislav Dukez <tomdu3@ymail.com>
2026-06-03 16:33:43 +0100

    add cors and socket base

--- server/index.js
+++ server/index.js
@@ -7,6 +7,21 @@ app.use(cors());
 
 const server = http.createServer(app);
 
+const io = new Server(server, {
+  cors: {
+    origin: 'https:://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`A user:${socket.id} connected.`);
+
+  socket.on("disconnect", () => {
+    console.log(`A user:${socket.id} disconnected.`);
+  });
+});
+
 server.listen(3001, () => {
   console.log("Server running.");
 });

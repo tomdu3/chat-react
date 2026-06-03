5489f80444c9d080492ad90ce38a66f4c92f6a52
Tomislav Dukez <tomdu3@ymail.com>
2026-06-03 17:06:35 +0100

    add placeholders hello world

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,15 @@
 import { useState } from 'react'
+import io from 'socket.io-client';
 import './App.css'
 
+const socket = io.connect('http://localhost:3000');
+
+
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <h1>Hello World</h1>
+
+  </div>;
 }
 
 export default App
